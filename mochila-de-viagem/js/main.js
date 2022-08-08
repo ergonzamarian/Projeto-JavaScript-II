4177ea59bd3f3be6eed453eee7372745669d0f78
ergonzamarian <ergonzamarian@gmail.com>
2022-08-07 21:10:39 -0400

    utilizando o localStorage para salvar as propriedades do formulário

--- mochila-de-viagem/js/main.js
+++ mochila-de-viagem/js/main.js
@@ -1,1 +1,47 @@
-console.log(document.getElementById("novoItem"))
+const form = document.getElementById("novoItem");
+const lista = document.getElementById("lista");
+const itens = JSON.parse(localStorage.getItem("itens")) || [];
+
+
+itens.forEach(element => {
+    console.log(element.nome, element.quantidade);
+});
+
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault();
+
+    const nome = evento.target.elements['nome'];
+    const quantidade = evento.target.elements['quantidade'];
+
+    criaElemento(nome.value, quantidade.value);
+    nome.value = "";
+    quantidade.value = "";
+    
+})
+
+function criaElemento(nome, quantidade) {
+
+    console.log(nome);
+    console.log(quantidade);
+
+    const novoItem = document.createElement('li');
+    novoItem.classList.add("item");
+
+    const numeroItem = document.createElement('strong');
+    numeroItem.innerHTML = quantidade;
+
+    novoItem.appendChild(numeroItem);
+    novoItem.innerHTML += nome;
+
+    lista.appendChild(novoItem);
+
+    const itemAtual = {
+        "nome": nome, 
+        "quantidade": quantidade
+    };
+
+    itens.push(itemAtual);
+
+    localStorage.setItem("item", JSON.stringify(itens));
+
+}
